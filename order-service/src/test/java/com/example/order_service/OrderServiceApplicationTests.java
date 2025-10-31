f8934dfcd8043ca33c5ddd87c426075547868c74
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2025-10-31 20:31:06 +0700

    update microservice

--- order-service/src/test/java/com/example/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/order_service/OrderServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.order_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "eureka.client.enabled=false" // Menonaktifkan Eureka Client saat test
+})
 class OrderServiceApplicationTests {
 
 	@Test
--- product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/product_service/ProductServiceApplicationTests.java
@@ -2,8 +2,13 @@ package com.example.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+
+@TestPropertySource(properties = {
+    "eureka.client.enabled=false" // Menonaktifkan Eureka Client saat test
+})
 class ProductServiceApplicationTests {
 
 	@Test
