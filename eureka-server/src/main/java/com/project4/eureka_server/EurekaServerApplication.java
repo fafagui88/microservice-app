3dbce05515476365af7022118b7a9f38457061dd
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2025-11-07 01:37:49 +0700

    update project 4

---
+++ eureka-server/src/main/java/com/project4/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.project4.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ histories/src/test/java/com/project4/histories/HistoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project4.histories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
